865da586cb97eb8e44aa24ecd576b4a306fcdc4d
Mikhail Koryakin <sdrr87@gmail.com>
2023-11-30 16:46:38 +0300

    feat: damn cosinus

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -106,8 +106,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,-1) (1,0)    => π/2
  *   (0,1) (0,1)     => 0
  */
-function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getAngleBetweenVectors(x1, y1, x2, y2) {
+  const cos =
+    (x1 * x2 + y1 * y2) / Math.sqrt((x1 ** 2 + y1 ** 2) * (x1 ** 2 + y1 ** 2));
+  return Math.acos(cos);
 }
 
 /**
